f46dcacdbbe23e610aafaae1fa22c661725d7a08
brianhoang <hoangbr@oregonstate.edu>
2019-10-30 19:03:18 -0700

    5b raw

--- Taxicab.py
+++ Taxicab.py
@@ -1,17 +1,17 @@
 class Taxicab:
-    def __init__(self, x_coord, y_coord, odometer):
-        self.x_coord = x_coord
-        self.y_coord = y_coord
-        self.odometer = odometer
+    def __init__(self,x_coord,y_coord):
+        self._x_coord=x_coord
+        self._y_coord=y_coord
+        self._odometer=odometer
     def get_x_coord(self):
-        return self.x_coord
+        return self._x_coord
     def get_y_coord(self):
-        return self.y_coord
+        return self._y_coord
     def get_odometer(self):
-        return self.y_coord
+        return self._y_coord
     def move_x(self,xmove):
-        self.odometer+=xmove
-        self.x_coord+=xmove
+        self._odometer+=xmove
+        self._x_coord+=xmove
     def move_y(self,ymove):
-        self.odometer+=ymove
-        self.y_coord+=ymove
+        self._odometer+=ymove
+        self._y_coord+=ymove
